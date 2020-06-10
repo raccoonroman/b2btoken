bbd8de1954382a12f7b4a7f66a280fb73a7225b4
Роман <kromanjs@gmail.com>
2020-06-10 16:51:35 +0300

    bug fix

---
+++ src/js/components/play-video.js
@@ -0,0 +1,11 @@
+const playVideoBtn = document.querySelectorAll('[data-video-play]');
+
+playVideoBtn.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    const videoBox = btn.closest('[data-video-box]');
+    const overlay = videoBox.querySelector('[data-video-overlay]');
+    btn.closest('[data-video-overlay]').remove();
+  });
+});
+
+
