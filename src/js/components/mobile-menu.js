94cc1056c33d9a97813361ab8b99aa686f17bca0
Роман <kromanjs@gmail.com>
2020-06-10 14:19:13 +0300

    add new section

--- src/js/components/mobile-menu.js
+++ src/js/components/mobile-menu.js
@@ -1,7 +1,11 @@
 const hamburger = document.getElementById('hamburger');
 const headerNav = document.getElementById('header-nav');
-
+const closeMenuBtn = document.getElementById('header-close');
 
 hamburger.addEventListener('click', () => {
   headerNav.classList.add('active');
+});
+
+closeMenuBtn.addEventListener('click', () => {
+  headerNav.classList.remove('active');
 });
