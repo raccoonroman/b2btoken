01e1cdd69dd4d43c477ccb8ce6617be7dca391fa
Роман <kromanjs@gmail.com>
2020-06-16 13:20:45 +0300

    minor improvements

--- src/js/components/mobile-menu.js
+++ src/js/components/mobile-menu.js
@@ -2,10 +2,13 @@ const hamburger = document.getElementById('hamburger');
 const headerNav = document.getElementById('header-nav');
 const closeMenuBtn = document.getElementById('header-close');
 
-hamburger.addEventListener('click', () => {
-  headerNav.classList.add('active');
-});
+if (headerNav) {
+  hamburger.addEventListener('click', () => {
+    headerNav.classList.add('active');
+  });
 
-closeMenuBtn.addEventListener('click', () => {
-  headerNav.classList.remove('active');
-});
+  closeMenuBtn.addEventListener('click', () => {
+    headerNav.classList.remove('active');
+  });
+}
+
